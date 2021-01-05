f0d33e859b65d243e265afcb738079d27f559f02
Damian Nogueiras <damiannogueiras@gmail.com>
2021-01-05 07:50:42 +0100

    Cambio de definiciones a fichero .h

--- include/jacsesp.h
+++ include/jacsesp.h
@@ -4,6 +4,56 @@
 *
 */
 
+#include <FS.h>
+
+#include <ESP8266WiFi.h>
+
+// upload csv file
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+// osc server
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+#include <SLIPEncodedSerial.h>
+
+// salida por terminal
+// Importante a false para que no envie al mega los mensajes de debug
+#define DEBUG false
+
+// leds informacion
+#define ROJO D0
+#define AZUL D1
+#define VERDE D2
+
+char ssid[] = "jacs";            // your network SSID (name)
+char pass[] = "perlitalagatita"; // your network password
+const char *hostName = "JACSesp";
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP Udp;
+const IPAddress outIp(192, 168, 1, 33); // remote IP (not needed for receive)
+const unsigned int outPort = 8888;      // remote port (not needed for receive)
+const unsigned int localPort = 8888;    // local port to listen for UDP packets (here's where we send the packets)
+
+// comunicacion serie con el arduino mega
+SLIPEncodedSerial SLIPSerial(Serial);
+OSCBundle bundleOUT;
+OSCErrorCode error;
+// esperamos confirmacion de que el mega puede recibir
+bool ack = true;
+
+// web server
+ESP8266WebServer server(80);
+// a File object
+File fsUploadFile;
+// file name to store frames, siempre el mismo
+String filename = "frames.csv";
+String getContentType(String filename); // convert the file extension to the MIME type
+bool handleFileRead(String path);       // send the right file to the client (if it exists)
+void handleFileUpload();                // upload a new file to the SPIFFS
+
 // number of fields in the csv file
 #define FIELDSIZE 11
 /* Fields
--- src/jacsesp.cpp
+++ src/jacsesp.cpp
@@ -11,57 +11,7 @@
   $ pio run -t uploadfs
 --------------------------------------------------------------------------------------------- */
 
-#include <FS.h>
-
-#include <ESP8266WiFi.h>
-
-// upload csv file
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include <WiFiManager.h>
-// osc server
-#include <WiFiUdp.h>
-#include <OSCMessage.h>
-#include <OSCBundle.h>
-#include <SLIPEncodedSerial.h>
-
 #include "jacsesp.h"
-
-// salida por terminal
-// Importante a false para que no envie al mega los mensajes de debug
-#define DEBUG false
-
-// leds informacion
-#define ROJO D0
-#define AZUL D1
-#define VERDE D2
-
-char ssid[] = "jacs";            // your network SSID (name)
-char pass[] = "perlitalagatita"; // your network password
-const char *hostName = "JACSesp";
-
-// A UDP instance to let us send and receive packets over UDP
-WiFiUDP Udp;
-const IPAddress outIp(192, 168, 1, 33); // remote IP (not needed for receive)
-const unsigned int outPort = 8888;      // remote port (not needed for receive)
-const unsigned int localPort = 8888;    // local port to listen for UDP packets (here's where we send the packets)
-
-// comunicacion serie con el arduino mega
-SLIPEncodedSerial SLIPSerial(Serial);
-OSCBundle bundleOUT;
-OSCErrorCode error;
-// esperamos confirmacion de que el mega puede recibir
-bool ack = true;
-
-// web server
-ESP8266WebServer server(80);
-// a File object
-File fsUploadFile;
-// file name to store frames, siempre el mismo
-String filename = "frames.csv";
-String getContentType(String filename); // convert the file extension to the MIME type
-bool handleFileRead(String path);       // send the right file to the client (if it exists)
-void handleFileUpload();                // upload a new file to the SPIFFS
 
 String getContentType(String filename)
 { // convert the file extension to the MIME type
@@ -191,8 +141,8 @@ size_t readField(File* file, char* str, size_t size, char* delim) {
 }
 
 /*
- * Read frames file and send bundle osc to mega
- * One bundle per line (frame)
+ * Read frames file and send osc to mega
+ * One msg per line (frame)
  */
 bool sendFramesMega()
 {
